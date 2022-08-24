dee842afc4aa0b808457c5e0b6f9e057b35e3084
Chenchang Xu <59164279+andyccxu@users.noreply.github.com>
2022-08-24 11:06:41 -0400

    Update make.sh

--- KnowledgeRetrieval/faster_rcnn/lib/make.sh
+++ KnowledgeRetrieval/faster_rcnn/lib/make.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 
+# NOTE: make sure that your CUDA PATH is the same as specified in this make file.
+
 # CUDA_PATH=/usr/local/cuda/
 
-export CUDA_PATH=/usr/local/cuda/
-#You may also want to ad the following
+export CUDA_PATH=/usr/local/cuda-9.0
+#You may also want to add the following
 #export C_INCLUDE_PATH=/opt/cuda/include
+
+# export CUDA PATH, otherwise nvcc command cannot be found
+export PATH="/usr/local/cuda-9.0/bin:$PATH"
+export LD_LIBRARY_PATH="/usr/local/cuda-9.0/lib64:$LD_LIBRARY_PATH"
 
 export CXXFLAGS="-std=c++11"
 export CFLAGS="-std=c99"
@@ -12,12 +18,14 @@ export CFLAGS="-std=c99"
 python setup.py build_ext --inplace
 rm -rf build
 
-CUDA_ARCH="-gencode arch=compute_30,code=sm_30 \
-           -gencode arch=compute_35,code=sm_35 \
-           -gencode arch=compute_50,code=sm_50 \
-           -gencode arch=compute_52,code=sm_52 \
-           -gencode arch=compute_60,code=sm_60 \
-           -gencode arch=compute_61,code=sm_61 "
+# CUDA_ARCH="-gencode arch=compute_30,code=sm_30 \
+#            -gencode arch=compute_35,code=sm_35 \
+#            -gencode arch=compute_50,code=sm_50 \
+#            -gencode arch=compute_52,code=sm_52 \
+#            -gencode arch=compute_60,code=sm_60 \
+#            -gencode arch=compute_61,code=sm_61 "\
+
+CUDA_ARCH="-gencode arch=compute_61,code=sm_61 "
 
 # compile NMS
 cd model/nms/src
@@ -35,7 +43,7 @@ echo "Compiling roi pooling kernels by nvcc..."
 nvcc -c -o roi_pooling.cu.o roi_pooling_kernel.cu \
 	 -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC $CUDA_ARCH
 cd ../
-python build.py
+# python build.py
 
 # compile roi_align
 cd ../../
